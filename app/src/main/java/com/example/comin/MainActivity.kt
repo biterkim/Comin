aef5b710784d4db57a661e00f5f74308b8a0d3ad
BuildTools <unconfigured@null.spigotmc.org>
2020-04-20 19:14:54 +0900

    aa

--- app/src/main/java/com/example/comin/MainActivity.kt
+++ app/src/main/java/com/example/comin/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity(){
     private lateinit var auth: FirebaseAuth// ...
 // Initialize Firebase Auth
 
-//a
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
