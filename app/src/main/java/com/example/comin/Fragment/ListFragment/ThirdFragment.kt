f9382adeac3f4da307496f56f7d3cc0f0ec9f675
BuildTools <unconfigured@null.spigotmc.org>
2020-04-25 16:15:30 +0900

    보물도시락 부분 수정 - 보물도시락 플래그먼트, 어뎁터, 레이아웃 추가 - 보물도시락 이미지 추가 (보물도시락의 마요만 추가 [한솥참고])

--- app/src/main/java/com/example/comin/Fragment/ListFragment/ThirdFragment.kt
+++ app/src/main/java/com/example/comin/Fragment/ListFragment/ThirdFragment.kt
@@ -1,11 +1,15 @@
 package com.example.comin.Fragment.ListFragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.comin.Fragment.MarketInfo.MarketInfoActivity
 import com.example.comin.R
+import com.example.comin.Utils.FirebaseUtils
+import kotlinx.android.synthetic.main.fragment_third.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -17,7 +21,68 @@ class ThirdFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third, container, false)
+        val view :View =inflater.inflate(R.layout.fragment_third, container, false);
+
+        val list_array = arrayListOf<ContentsListModel>(
+
+            ContentsListModel(R.drawable.list3_1,"메가불닭치킨 고로케",1,""),
+            ContentsListModel(R.drawable.list3_2,"메가불닭참치 고로케",1,"d"),
+            ContentsListModel(R.drawable.list3_3,"메가불닭치킨 케이준",1,"d"),
+            ContentsListModel(R.drawable.list3_4,"메가불닭참치 케이준",1,"d"),
+            ContentsListModel(R.drawable.list3_5,"메가불닭치킨마요",1,"d"),
+            ContentsListModel(R.drawable.list3_6,"메가불닭참치마요",1,"d"),
+            ContentsListModel(R.drawable.list3_7,"피자불고기마요",1,"d"),
+            ContentsListModel(R.drawable.list3_8,"피자치킨마요",1,"d"),
+            ContentsListModel(R.drawable.list3_9,"갈비치킨마요",1,"d")
+        )
+
+        val list_adapter =
+            FirstFragAdapter(
+                requireContext(),
+                list_array
+            )
+        view.listview_third_fragment.adapter = list_adapter
+
+        FirebaseUtils.db
+            .collection("zzim")
+            .document(FirebaseUtils.getUid())
+            .get()
+            .addOnSuccessListener { documentSnapshot ->
+                if (documentSnapshot.exists() == true){
+
+                }else{
+                    val lecture = hashMapOf(
+                        "메가불닭치킨 고로케" to "",
+                        "메가불닭참치 고로케" to "",
+                        "메가불닭치킨 케이준" to "",
+                        "메가불닭참치 케이준" to "",
+                        "메가불닭치킨마요" to "",
+                        "메가불닭참치마요" to "",
+                        "피자불고기마요" to "",
+                        "피자치킨마요" to "",
+                        "갈비치킨마요" to ""
+                    )
+
+                    FirebaseUtils.db
+                        .collection("zzim")
+                        .document(FirebaseUtils.getUid())
+                        .set(lecture)
+                        .addOnSuccessListener {  }
+                        .addOnFailureListener {  }
+
+                }
+            }
+            .addOnFailureListener {  }
+
+        view.listview_third_fragment.setOnItemClickListener { adapterView, view,i, l ->
+
+            val intent = Intent(requireContext(), MarketInfoActivity::class.java)
+            intent.putExtra("title",list_array.get(i).title)
+            startActivity(intent)
+
+        }
+
+        return view
     }
 
 }
