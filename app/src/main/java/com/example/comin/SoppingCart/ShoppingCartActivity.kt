a76b899b2a8a6837e44b8c28474d90dd798740fd
Hye2Zy <ansxoddl16@naver.com>
2020-05-08 22:54:10 +0900

    Merge branch 'master' of D:\Bamin with conflicts.

--- app/src/main/java/com/example/comin/SoppingCart/ShoppingCartActivity.kt
+++ app/src/main/java/com/example/comin/SoppingCart/ShoppingCartActivity.kt
@@ -3,11 +3,17 @@ package com.example.comin.SoppingCart
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.comin.R
+import kotlinx.android.synthetic.main.activity_shopping_cart.*
 
 class ShoppingCartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping_cart)
+
+        //결제하기 버튼 클릭
+        pay_button.setOnClickListener {
+
+        }
     }
 }
