8e346a2446bec1f72928a8f2b3db42c2ac8835a8
Hye2Zy <ansxoddl16@naver.com>
2020-05-08 23:38:47 +0900

    결제 기능

--- app/src/main/java/com/example/comin/SoppingCart/KakaoPayActivity.kt
+++ app/src/main/java/com/example/comin/SoppingCart/KakaoPayActivity.kt
@@ -20,7 +20,7 @@ import kr.co.bootpay.model.BootExtra
 import kr.co.bootpay.model.BootUser
 
 class KakaoPayActivity : AppCompatActivity() {
-
+ //
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kakao_pay)
