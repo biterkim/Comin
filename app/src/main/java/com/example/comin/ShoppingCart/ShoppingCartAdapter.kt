a0d5e2cd732e3a3dc90db4ddd5ac1e8fa791c859
BuildTools <unconfigured@null.spigotmc.org>
2020-05-18 21:14:04 +0900

    장바구니 데이터베이스 연동 및 출력 -삭제 추가해야함 -이미지는 고려

--- app/src/main/java/com/example/comin/ShoppingCart/ShoppingCartAdapter.kt
+++ app/src/main/java/com/example/comin/ShoppingCart/ShoppingCartAdapter.kt
@@ -7,13 +7,18 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import com.example.comin.R
 import kotlinx.android.synthetic.main.shopping_cart_item.view.*
-
 //img:음식 사진 name:음식 이름, price:음식 가격, total_price:음식 별 총금액, delete_button: 삭제버튼,
-class ShoppingCartAdapter(val context : Context, val list_array : ArrayList<String>):BaseAdapter() {
+class ShoppingCartAdapter(val context : Context,
+                          var list_menu : List<String>,
+                          var list_count : List<String>) : BaseAdapter() {
+
 
     override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+
         val view : View = LayoutInflater.from(context).inflate(R.layout.shopping_cart_item,null)
-        view.shopping_cart_text.text = list_array.get(p0)
+
+        view.menu_name.text = list_menu.get(p0)
+        view.menu_count.text = list_count.get(p0)
         return view
     }
 
@@ -26,6 +31,6 @@ class ShoppingCartAdapter(val context : Context, val list_array : ArrayList<Stri
     }
 
     override fun getCount(): Int {
-        return list_array.size
+        return list_menu.size
     }
 }
